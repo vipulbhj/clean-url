bbf22ed11ea8de620ab579753fc6fb56107dd334
vipulbhj <2015pcecsvipul119@poornima.org>
2019-01-12 12:58:44 +0530

    Tests added

--- tests/app.js
+++ tests/app.js
@@ -11,11 +11,23 @@ const test = [
 ]
 
 
+function printTestInfo(url, expected, output) {
+    const templateStr = `
+        Running test for ${url}
+        Status ${expected === output ? 'SUCCESS': 'FAILED'}
+        Expected Result ${expected}
+        Output ${output}
+    `;
+
+    console.log(templateStr);
+}
+
+
+console.log('The Urls are tested with checkCleanUrl function exposed by this module');
 
 console.log("Starting  Test Module");
-
 for(let i of test) {
-    console.log('Testing for',i[0],checkCleanUrl(i[0]), i[1]);
+    printTestInfo(i[0], i[1], checkCleanUrl(i[0]));
 }
 
 console.log("Test Module Finished");
